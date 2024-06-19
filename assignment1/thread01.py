c39ccbb7a575e93e57adc9001578b93481ba5cab
tosapat <6510301002@cdti.ac.th>
2024-06-19 13:38:37 +0700

    submit 01 assignment

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -3,18 +3,18 @@ from time import sleep,ctime
 from threading import Thread
 
 # a custom fuction that blocks for a moment
-def task():
+def task(): # สร้างเพื่อใช้งาน thread ใหม่
     # block for a moment
-    sleep(1)
-    # display a message 
-    print (f'{ctime()} This is from another thread')
+    sleep(1) # หยุดการทำงาน 1 sec
+    # display a message  
+    print (f'{ctime()} This is from another thread') # แสดงข้อความพร้อมเวลาปัจจุบัน
 
 # create a thread
-thread = Thread (target=task)
+thread = Thread (target=task) # การสร้าง thread 
 # run the thread
 thread.start()
 # wait for the thread to finish
-print (f'{ctime()} Waiting for the thread...')
+print (f'{ctime()} Waiting for the thread...') # ตัวนี้จะแสดงก่อนเพราะไม่ต้องรอแต่ทั้งสองตัวทำงานพร้อมกัน
 thread.join() 
 
 
