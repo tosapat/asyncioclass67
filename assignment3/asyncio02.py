6250d7bf59de8f2aab859078250727df83f6819c
tosapat <6510301002@cdti.ac.th>
2024-06-26 12:13:53 +0700

    submit as2,as3

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,13 @@
 # example of running a coroutine
+import asyncio
+#define a corotine
+async def custom_coro():
+    #await another coroutine
+    await asyncio.sleep(1)
 
+#main coroutine
+async def main():
+    #execute my custom coroutine
+    await custom_coro()
+#start the coroutine program
+    asyncio.run(main())
