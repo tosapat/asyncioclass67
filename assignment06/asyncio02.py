b4399124b09e60b69ebb2233f00091de9e135931
tosapat <6510301002@cdti.ac.th>
2024-07-17 11:05:16 +0700

    ส่งงาน ass6

---
+++ assignment06/asyncio02.py
@@ -0,0 +1,24 @@
+import asyncio
+
+class AsyncIterator:
+    def __init__(self):
+        self.counter = 0
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if self.counter >= 10:
+            raise StopAsyncIteration
+
+        self.counter += 1
+
+        await asyncio.sleep(1)
+
+        return self.counter
+
+async def main():
+    async for item in AsyncIterator():
+        print(item)
+
+asyncio.run(main())
